44aaa40759fd30bb60a2fba169035bc9ab8f0a8f
Juan Sulca <juanm.sulca@gmail.com>
2020-05-31 16:55:58 -0500

    added simple todo CRUD

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  HttpException,
+  HttpStatus,
+  Post,
+  Body,
+  Delete,
+  HttpCode,
+} from '@nestjs/common';
 
 @Controller('todos')
-export class TodosController {}
+export class TodosController {
+  @Get()
+  findAll() {
+    return [
+      {
+        id: 1,
+        name: 'task 1',
+        done: false,
+      },
+    ];
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    return {
+      id,
+      name: '',
+      done: false,
+    };
+  }
+
+  @Post()
+  @HttpCode(200)
+  create(@Body() payload: any) {
+    return payload;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: number) {
+    throw new HttpException({ id , notImplemented: true}, HttpStatus.NOT_FOUND);
+  }
+}
